5b4117dc615c6cd60e1a799db7a0a6ecfad782fb
Sevy Harris <harris.se@northeastern.edu>
2021-11-01 11:25:47 -0400

    add Cyclopentadiene_scission rule sensitivities

--- input/kinetics/families/Cyclopentadiene_scission/rules.py
+++ input/kinetics/families/Cyclopentadiene_scission/rules.py
@@ -10,7 +10,7 @@ entry(
     index = 1,
     label = "Root",
     kinetics = ArrheniusBM(A=(1.45e+12,'s^-1'), n=0.194, w0=(685000,'J/mol'), E0=(106333,'J/mol'), uncertainty=RateUncertainty(mu=0.0, var=33.13686319048999, Tref=1000.0, N=1, data_mean=0.0, correlation='Root',), comment="""BM rule fitted to 1 training reactions at node Root
-    Total Standard Deviation in ln(k): 11.540182761524994"""),
+        Total Standard Deviation in ln(k): 11.540182761524994"""),
     rank = 11,
     shortDesc = """BM rule fitted to 1 training reactions at node Root
 Total Standard Deviation in ln(k): 11.540182761524994""",
@@ -18,6 +18,7 @@ Total Standard Deviation in ln(k): 11.540182761524994""",
 """
 BM rule fitted to 1 training reactions at node Root
 Total Standard Deviation in ln(k): 11.540182761524994
+sensitivities = [{'dA': 0.9995003330837129, 'dE0': 0.0, 'dn': 0.0, 'dA_dEa': 0.0, 'dE0_dEa': 1.2909551972395585, 'dn_dEa': 0.0, 'name': 'C6H6 <=> C6H6-2'}]
 """,
 )
 
--- input/kinetics/families/Cyclopentadiene_scission/training/reactions.py
+++ input/kinetics/families/Cyclopentadiene_scission/training/reactions.py
@@ -2,8 +2,8 @@
 # encoding: utf-8
 
 name = "Cyclopentadiene_scission/training"
-shortDesc = u"Reaction kinetics used to generate rate rules"
-longDesc = u"""
+shortDesc = "Reaction kinetics used to generate rate rules"
+longDesc = """
 Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
 """
@@ -11,11 +11,11 @@ entry(
     index = 0,
     label = "C6H6 <=> C6H6-2",
     degeneracy = 1.0,
-    kinetics = Arrhenius(A=(1.45e+12, 's^-1'), n=0.194, Ea=(32.274, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1.45e+12,'s^-1'), n=0.194, Ea=(32.274,'kcal/mol'), T0=(1,'K')),
     rank = 5,
-    shortDesc = u"""Training reaction from kinetics library: 2003_Miller_Propargyl_Recomb_High_P""",
+    shortDesc = """Training reaction from kinetics library: 2003_Miller_Propargyl_Recomb_High_P""",
     longDesc = 
-u"""
+"""
 Taken from entry: B <=> IX
 """,
 )
